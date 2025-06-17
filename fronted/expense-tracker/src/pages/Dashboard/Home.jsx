8658919e22d545b63e6bea522be6017e5c887463
niiiit <101655868+niiiit@users.noreply.github.com>
2025-06-18 00:48:15 +0530

    integrated sign up and side menu

--- fronted/expense-tracker/src/pages/Dashboard/Home.jsx
+++ fronted/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout'
+import { useUserAuth } from '../../hooks/useUserAuth';
 
 const Home = () => {
+  useUserAuth();
   return (
     <DashboardLayout activeMenu="Dashboard">
-      <div className='my-5 mx-auto'>
+      <div className='my-5 mx-auto'>Home
       </div>
     </DashboardLayout>
   )
